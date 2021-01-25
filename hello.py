a7b5ccaf4f453c8f641c1a5622f21051d7c5f54b
Juan Cruz <jc23729@gmail.com>
2021-01-25 17:57:23 -0500

    update

--- hello.py
+++ hello.py
@@ -1,23 +1,6 @@
-from flask import Flask, render_template
-
-
+from flask import Flask
 app = Flask(__name__)
-"""
-To start the hello.py application from the previous section, first make sure the virtual environment you created earlier is activated and has Flask installed in it. For Linux and macOS users, start the web server as follows:
-(venv) $ export FLASK_APP=hello.py (venv) $ flask run
-* Serving Flask app "hello"
-* Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
-"""
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-# ##Dynamic Routes### The application will respond with the personalized greeting using the name dynamic argument. 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html, name=name')
-
-##Need to figure out if going to follow the book or the blog
-
-if __name__ == '__main__': app.run()
+    return '<h1>Hello World!</h1>'
